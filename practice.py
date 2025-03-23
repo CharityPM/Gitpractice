ea7466ab18afc86460acc9d97cd778bd680f2c4e
CharityPM <mwangangipauline8@gmail.com>
2025-03-23 04:28:52 +0300

    finally

--- practice.py
+++ practice.py
@@ -2,8 +2,6 @@ print("hello world")
 print("Im Charity")
 print("for practice")
 print("almost there")
-<<<<<<< HEAD
 print("yes yes")
-=======
 print("not yet")
->>>>>>> 340a1442a97fbc24be22e831b3c0f8a18cf82a52
+print("mmmh mmh")
